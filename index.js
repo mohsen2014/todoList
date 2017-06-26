2e393d70c4a8663a200a0879f6052c35bf612bda
mohsen2014 <moh.motamede@gmail.com>
2017-06-26 14:20:38 +0430

    login | signup validation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-  
+  bodyParser = require('body-parser');
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
