87fdbed65d3ee1dd82592a4da1ad2c6a2e4edcad
mohsen2014 <moh.motamede@gmail.com>
2017-06-26 16:52:19 +0430

    add loopback

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  passport = require('passport'),
+  config = require('./config');
+
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
@@ -7,11 +10,22 @@ app.use(express.static('./client/dist/'));
 app.use(express.static('./bower_components'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
 // routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
-
+app.use('/api', apiRoutes);
 
 // start the server
 app.listen(3000, () => {
